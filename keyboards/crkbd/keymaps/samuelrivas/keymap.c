9eac93e15f1a6302fbe8e12a942123da89b16d15
Samuel Rivas <samuelrivas@gmail.com>
2026-03-26 07:11:05 +0100

    add underglow controls

--- keyboards/crkbd/keymaps/samuelrivas/keymap.c
+++ keyboards/crkbd/keymaps/samuelrivas/keymap.c
@@ -24,12 +24,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    // Row 1, left
    KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, QK_BOOT,
    // Row 1, right
-   KC_NO, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_BSPC,
+   UG_NEXT, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_BSPC,
 
    // Row 2, left
    KC_LCTL, LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G, KC_NO,
    // Row 2, right
-   KC_NO, KC_M, RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), KC_QUOT,
+   UG_TOGG, KC_M, RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), KC_QUOT,
 
    // Row 3, left
    KC_LSFT, KC_Z, KC_X, KC_C, KC_D, KC_V,
