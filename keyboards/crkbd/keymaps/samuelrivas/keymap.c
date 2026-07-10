ef62449f4fad1a570ff14102372db247ce7cc416
Samuel Rivas <samuelrivas@gmail.com>
2026-07-10 19:15:27 +0200

    Add toggle to enter "Mac mode"
    Swapping alt and gui, so that mods behave the same on the mac with karabiner

--- keyboards/crkbd/keymaps/samuelrivas/keymap.c
+++ keyboards/crkbd/keymaps/samuelrivas/keymap.c
@@ -16,12 +16,31 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/*
+  This is based on the Miryoku layout: https://github.com/manna-harbour/miryoku,
+  but it has some customisations:
+
+  It supports English, Spanish and Swedish alphabets, using the international US
+  layout in the OS. We use the extra outer keys in the extended Corne layout to
+  add the extra letters. To keep it compact, we include a toggle key to choose
+  language, so that we only need 4 extra keys: 3 for the Swedish letters and one
+  for the toggle. Spanish only needs 2 extra letters.
+
+  We add a custom keycode to swap GUI and ALT to keep things according to our
+  preferences when using the keyboard on a Mac with a very bespoke Karabiner
+  config. As we need to keep Right ALT in place to support the extra language
+  keys, we assign left GUI and ALT to both left and right sides. We need a
+  custom keycode because the existing magic keycodes either toggle both side
+  swaps or enable/disable one side. We would need to use two keys, one to enable
+  and a different one to disable if we used the existing ones
+*/
+
 #include QMK_KEYBOARD_H
 
 void doubletap_ug_toggle(tap_dance_state_t *state, void *user_data);
 void doubletap_qk_boot(tap_dance_state_t *state, void *user_data);
 
-enum {
+enum my_layers {
   L_BASE,
   L_SE,
   L_NUM,
@@ -30,9 +49,13 @@ enum {
   L_F
 };
 
-enum {
+enum my_tap_dance {
   TD_UG_TOGG,
   TD_QK_BOOT,
+};
+
+enum my_custom_key_codes {
+  KC_MAC_TOGGLE = SAFE_RANGE
 };
 
 tap_dance_action_t tap_dance_actions[] = {
@@ -40,19 +63,31 @@ tap_dance_action_t tap_dance_actions[] = {
   [TD_QK_BOOT] = ACTION_TAP_DANCE_FN(doubletap_qk_boot),
 };
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case KC_MAC_TOGGLE:
+    if (record -> event.pressed) {
+      keymap_config.swap_lalt_lgui = !keymap_config.swap_lalt_lgui;
+      eeconfig_update_keymap(&keymap_config);
+    }
+    return false;
+  }
+  return true;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // base
   [L_BASE] = LAYOUT_split_3x6_3_ex2
   (
    // Row 1, left
-   KC_NO, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_NO,
+   KC_NO, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_MAC_TOGGLE,
    // Row 1, right
    UG_NEXT, KC_J, KC_L, KC_U, KC_Y, KC_QUOT, KC_NO,
 
    // Row 2, left
    RALT(KC_QUOT), LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G, KC_NO,
    // Row 2, right
-   UG_TOGG, KC_M, RSFT_T(KC_N), RCTL_T(KC_E), LALT_T(KC_I), RGUI_T(KC_O), RALT(KC_N),
+   UG_TOGG, KC_M, RSFT_T(KC_N), RCTL_T(KC_E), LALT_T(KC_I), LGUI_T(KC_O), RALT(KC_N),
 
    // Row 3, left
    KC_NO, KC_Z, KC_X, KC_C, KC_D, KC_V,
